d0d9e05d5fef1cef756ab44a18a7de258b96123c
wildonion <ea_pain@yahoo.com>
2020-01-25 23:46:52 +0330

    bug fix in loading logo

--- js/fetch/content.js
+++ js/fetch/content.js
@@ -10,8 +10,8 @@ if(slug && id && (lang == 'en' || lang == 'fa')){
     fetch(`https://panel.cdsapply.com:2017/api/collections/get/post?token=account-3eb37339b9641b90e3f0b73b7cedf6&filter[_id]=${id}`)
         .then(response => response.json())
         .then(posts => {
-            document.getElementById("main_backimg").setAttribute("data-image-src", "https://panel.cdsapply.com:2017"+posts[0].logo.path)
             posts = posts.entries
+            document.getElementById("main_backimg").setAttribute("data-image-src", "https://panel.cdsapply.com:2017"+posts[0].logo.path)
             	 $('meta[name=description]').remove();
             	 $('title').remove();
         		 $('head').append(`<meta name="description" content=${posts[0].tags.join()}>`);
