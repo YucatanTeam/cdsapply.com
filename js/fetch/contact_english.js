cd9647571d02a7019c570fcd1a4c1ea01596194a
2017-A-15 <ea_pain@yahoo.com>
2020-02-28 16:42:27 +0330

    big updated

---
+++ js/fetch/contact_english.js
@@ -0,0 +1,25 @@
+
+fetch('https://panel.cdsapply.com:2017/api/collections/get/contactUS?token=account-3eb37339b9641b90e3f0b73b7cedf6')
+.then(response => response.json())
+.then(contactUS => {
+    contactUS = contactUS.entries
+    document.getElementById("contactus").innerHTML = contactUS[0].en_content
+    images = document.getElementById('contactus').getElementsByTagName('img')
+    for(i = 0; i < images.length; i++){
+        IMGsrc = document.getElementById('contactus').getElementsByTagName('img')[i].src
+        IMGsrc = IMGsrc.replace('https://cdsapply.com',  'https://panel.cdsapply.com:2017')
+        document.getElementById('contactus').getElementsByTagName('img')[i].setAttribute("src", IMGsrc)
+    }
+})
+
+// fetch seo
+fetch('https://panel.cdsapply.com:2017/api/collections/get/seo?token=account-3eb37339b9641b90e3f0b73b7cedf6&filter[page]=contact')
+.then(response => response.json())
+.then(seo => {
+    seo = seo.entries
+    $('meta[name=description]').remove();
+    $('head').append(`<meta name="description" content=${seo[0].tags.join()}>`);
+    for(i = 0; i < seo[0].tags.length; i++){
+        $('head').append(`<meta name="description" content=${seo[0].tags[i]}>`);
+    }
+})
