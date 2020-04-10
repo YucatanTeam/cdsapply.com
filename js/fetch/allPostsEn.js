7042c8969c7dd51fddb4594eca95a304484f584d
2017-A-15 <ea_pain@yahoo.com>
2020-04-10 09:54:45 +0430

    fix show all posts issue

---
+++ js/fetch/allPostsEn.js
@@ -0,0 +1,65 @@
+// fetch posts
+fetch('https://panel.cdsapply.com:2017/api/collections/get/post?token=account-3eb37339b9641b90e3f0b73b7cedf6&sort[_created]=-1')
+      .then(response => response.json())
+      .then(posts => {
+        posts = posts.entries
+
+        for(i = 0; i < posts.length; i++){
+
+                if(posts[i].en_title){
+
+            	divCard = document.createElement("div")
+            	divCard.classList.add("col-sm-6", "col-md-4", "col-lg-3", "mt-4")
+
+            	cardLink = document.createElement("a")
+            	cardLink.href = "https://cdsapply.com/content.html?slug="+posts[i].en_slug+"&id="+posts[i]._id+"&lang=fa"
+            	cardLink.classList.add("card")
+
+            	cardImg = document.createElement("img")
+            	cardImg.classList.add("card-img-top")
+            	cardImg.setAttribute("src", "https://panel.cdsapply.com:2017"+posts[i].logo.path)
+
+            	secDiv = document.createElement("div")
+            	secDiv.classList.add("card-block")
+
+            	cardHeader = document.createElement("h4")
+            	cardHeader.classList.add("card-title", "mt-3", "saheltitle")
+            	cardHeader.appendChild(document.createTextNode(posts[i].en_title))
+                cardHeader.setAttribute("style", "direction: ltr;")
+                
+
+            	thDiv = document.createElement("div")
+            	thDiv.classList.add("card-text")
+            	thDiv.innerHTML = posts[i].en_content
+                thDiv.innerHTML = thDiv.textContent.slice(0,80) + ' ...'
+
+                fDiv = document.createElement("div")
+                fDiv.classList.add("card-footer")
+
+                cardTime = document.createElement("small")
+                date = new Date(posts[i]._created)
+                var time = '';
+                time += date.getUTCDate()-1 + " days - ";
+                time += date.getUTCHours() + " hours - ";
+                time += date.getUTCMinutes() + " minutes ago ";
+                cardTime.innerHTML = time
+                cardTime.setAttribute("dir", "ltr")
+
+
+
+                divCard.appendChild(cardLink)
+                cardLink.appendChild(cardImg)
+                cardLink.appendChild(secDiv)
+                secDiv.appendChild(cardHeader)
+                secDiv.appendChild(thDiv)
+                cardLink.appendChild(fDiv)
+                fDiv.appendChild(cardTime)
+
+
+                document.getElementById("postCards").appendChild(divCard)
+
+            } else {
+                $(location).attr('href','https://cdsapply.com')
+            }
+        }
+    })
--- js/fetch/cards.js
+++ js/fetch/cards.js
@@ -5,6 +5,9 @@ const country = urlParams.get("country")
 const lang = urlParams.get("lang")
 
 
+if(lang == "fa") $('head').append(`<title> موسسه کندو  - CDS Apply | ${country}</title>`);
+if(lang == "en") $('head').append(`<title>CANDO Study Abroad Agency | ${country}</title>`);
+
 fetch_cmd = null
 if(country != '') fetch_cmd = fetch(`https://panel.cdsapply.com:2017/api/collections/get/post?token=account-3eb37339b9641b90e3f0b73b7cedf6&filter[country]=${country}&sort[_created]=-1&limit=4`)
 
@@ -12,33 +15,42 @@ if(fetch_cmd && (lang == 'en' || lang == 'fa')){
     fetch_cmd
         .then(response => response.json())
         .then(posts => {
-        	var cards = document.getElementById("cards")
+
         	posts = posts.entries
             if(posts.length==0) $(location).attr('href','https://cdsapply.com')
-        	if(lang == "fa") $('head').append(`<title> موسسه کندو  - CDS Apply | ${country}</title>`);
-            if(lang == "en") $('head').append(`<title>CANDO Study Abroad Agency | ${country}</title>`);
-            
+        	
             for(i = 0; i < posts.length; i++){
-        		var container = document.createElement("div")
-                container.setAttribute("class", "card")
-        		container.classList.add("card", "col-lg-6", "mt-3")
-        		container.setAttribute("style", "width: 18rem;")
+        		
+                divCard = document.createElement("div")
+                divCard.classList.add("col-sm-6", "col-md-4", "col-lg-3", "mt-4")
 
-        		var cardImg = document.createElement("img")
-        		cardImg.classList.add("card-img-top")
-        		cardImg.setAttribute("src", "https://panel.cdsapply.com:2017"+posts[i].logo.path)
+                cardLink = document.createElement("a")
+                cardLink.classList.add("card")
 
-        		var cardBody = document.createElement("div")
-        		cardBody.classList.add("card-body")
+                cardHeader = document.createElement("h4")
+                cardHeader.classList.add("card-title", "mt-3", "saheltitle")
+                
+                cardImg = document.createElement("img")
+                cardImg.classList.add("card-img-top")
+                cardImg.setAttribute("src", "https://panel.cdsapply.com:2017"+posts[i].logo.path)
 
-        		var cardHeader = document.createElement("h5")
-        		cardHeader.classList.add("card-title")
+                secDiv = document.createElement("div")
+                secDiv.classList.add("card-block")
 
-        		var cardContent = document.createElement("p")
-        		cardContent.classList.add("card-text")
+                thDiv = document.createElement("div")
+                thDiv.classList.add("card-text")
 
-        		var cardLink = document.createElement("a")
-        		cardLink.classList.add("btn", "btn-warning")
+                fDiv = document.createElement("div")
+                fDiv.classList.add("card-footer")
+
+                cardTime = document.createElement("small")
+                date = new Date(posts[i]._created)
+                var time = '';
+                time += date.getUTCDate()-1 + " days - ";
+                time += date.getUTCHours() + " hours - ";
+                time += date.getUTCMinutes() + " minutes ago ";
+                cardTime.innerHTML = time
+                
 
 
         		if(lang == "en" && posts[i].en_title != ""){
@@ -46,39 +58,43 @@ if(fetch_cmd && (lang == 'en' || lang == 'fa')){
     	    		for(j = 0; j < posts[i].tags.length; j++){
     		            $('head').append(`<meta name="description" content=${posts[i].tags[i]}>`);
     		        }
-        			cardImg.setAttribute("alt", posts[i].en_title)
-        			cardHeader.appendChild(document.createTextNode(posts[i].en_title))
-        			cardHeader.setAttribute("style", "font-weight: bold; font-size: 30px;")
-        			cardContent.innerHTML = posts[i].en_content
-                    cardContent.innerHTML = cardContent.textContent.slice(0,80) + ' ...'
+
+                    cardHeader.appendChild(document.createTextNode(posts[i].en_title))
+                    thDiv.innerHTML = posts[i].en_content
+                    thDiv.innerHTML = thDiv.textContent.slice(0,80)+'...'
         			cardLink.href = "https://cdsapply.com/content.html?slug="+posts[i].en_slug+"&id="+posts[i]._id+"&lang=en"
-        			cardLink.appendChild(document.createTextNode("Read More"))
-                    cardLink.setAttribute("style", "margin-top: 15px;")
+                    cardTime.setAttribute("style", "float: left")
+                    cardHeader.setAttribute("style", "float: left")
+
         		}
         		else if(lang == "fa" && posts[i].title != ""){
         			$('head').append(`<meta name="description" content=${posts[i].tags.join()}>`);
         			for(j = 0; j < posts[i].tags.length; j++){
     		            $('head').append(`<meta name="description" content=${posts[i].tags[i]}>`);
     		        }
-        			cardImg.setAttribute("alt", posts[i].title)
-        			cardHeader.appendChild(document.createTextNode(posts[i].title))
-        			cardHeader.setAttribute("style", "font-weight: bold; font-size: 30px; float:right")
-        			cardContent.innerHTML = posts[i].content
-                    cardContent.innerHTML = '... '+cardContent.textContent.slice(0,80)
-        			cardContent.setAttribute("style", "float: right;")
+
+                    cardHeader.appendChild(document.createTextNode(posts[i].title))
+                    thDiv.innerHTML = posts[i].content
+                    thDiv.innerHTML = thDiv.textContent.slice(0,80)+'...'
         			cardLink.href = "https://cdsapply.com/content.html?slug="+posts[i].slug+"&id="+posts[i]._id+"&lang=fa"
-        			cardLink.appendChild(document.createTextNode("ادامه مطلب"))
-        			cardLink.setAttribute("style", "float: right; margin-top: 15px;")
-        		} else{
+                    cardTime.setAttribute("style", "float: right")
+                    thDiv.setAttribute("style", "direction: rtl") // TODO : fix the floating font issue!!!!!!
+                    cardHeader.setAttribute("style", "float: right")
+        		
+                } else{
                     $(location).attr('href','https://cdsapply.com')
                 }
 
-        		container.appendChild(cardImg)
-        		container.appendChild(cardBody)
-        		cardBody.appendChild(cardHeader)
-        		cardBody.appendChild(cardContent)
-        		cardBody.appendChild(cardLink)
-        		cards.appendChild(container)
+        		divCard.appendChild(cardLink)
+                cardLink.appendChild(cardImg)
+                cardLink.appendChild(secDiv)
+                secDiv.appendChild(cardHeader)
+                secDiv.appendChild(thDiv)
+                cardLink.appendChild(fDiv)
+                fDiv.appendChild(cardTime)
+
+
+                document.getElementById("cards").appendChild(divCard)
         		
         	}
         })
